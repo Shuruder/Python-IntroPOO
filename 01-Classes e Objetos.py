c104999cd85b4fa81cab4303b401a4513b7b696a
shuruder <gabriel_brandini@hotmail.com>
2022-12-28 11:48:15 -0300

    Exemplos de ciração de classes e metodos

---
+++ 01-Classes e Objetos.py
@@ -0,0 +1,30 @@
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano 
+        self.valor = valor 
+        
+    def buzinar(self):
+        print("Trim Trim ...")
+    
+    def parar(self):
+        print("Parando Bicicleta...")
+        print("Bicicleta parada!")
+        
+    def correr(self):
+        print("Vruuuuuum...")
+        
+    #def __str__(self):
+    #    return  f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {''.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+        
+b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+
+#b1.buzinar
+#b1.parar
+#b1.correr
+
+print(b1)
