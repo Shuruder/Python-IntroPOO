4d15d8459954d158c2692cce2e19795cad7e2d00
shuruder <gabriel_brandini@hotmail.com>
2022-12-28 12:25:01 -0300

    Commit Final

--- 01-Classes e Objetos.py
+++ 01-Classes e Objetos.py
@@ -19,7 +19,7 @@ class Bicicleta:
     #    return  f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
     
     def __str__(self):
-        return f"{self.__class__.__name__}: {''.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
         
 b1 = Bicicleta("vermelha", "caloi", 2022, 600)
 
